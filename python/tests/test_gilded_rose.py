d3a8b8f824cfef9e9d7c6118cfeee4c03cbbd498
lmarnat14@gmail.com <lmarnat14@gmail.com>
2025-12-04 13:35:24 +0100

    refactor: move update_quality in Item class
    Also adds tests on items

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -7,11 +7,46 @@ from item import Item
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        items = [Item("foo", 0, 0)]
+        items = [Item("foo", 2, 10),
+                 Item("foo", 0, 10)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        self.assertEqual("foo", items[0].name)
+        self.assertEqual(9, items[0].quality)
+        self.assertEqual(1, items[0].sell_in)
+        self.assertEqual(8, items[1].quality)
+        self.assertEqual(-1, items[1].sell_in)
 
-        
+    def test_aged_brie(self):
+        items = [Item("Aged Brie", 2, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(1, items[0].sell_in)
+        self.assertEqual(1, items[0].quality)
+
+    def test_sulfuras(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].sell_in)
+        self.assertEqual(80, items[0].quality)
+
+    def test_backstage_passes(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                 Item("Backstage passes to a TAFKAL80ETC concert", 10, 20),
+                 Item("Backstage passes to a TAFKAL80ETC concert", 5, 20),
+                 Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(14, items[0].sell_in)
+        self.assertEqual(21, items[0].quality)
+        self.assertEqual(9, items[1].sell_in)
+        self.assertEqual(22, items[1].quality)
+        self.assertEqual(4, items[2].sell_in)
+        self.assertEqual(23, items[2].quality)
+        self.assertEqual(-1, items[3].sell_in)
+        self.assertEqual(0, items[3].quality)
+
+
 if __name__ == '__main__':
     unittest.main()
